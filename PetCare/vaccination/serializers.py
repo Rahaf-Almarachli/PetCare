861d8f2ca369283d246c77b84b17c1efbd5f73a4
Rahaf-Almarachli <ririkimv@gmail.com>
2025-09-19 16:15:22 -0600

    joiho

--- PetCare/vaccination/serializers.py
+++ PetCare/vaccination/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from .models import Vaccination
-from pets.models import Pet  # يجب استيراد نموذج الحيوان الأليف
+from pets.models import Pet
 
 class VaccinationSerializer(serializers.ModelSerializer):
-
+    # حقل للقراءة فقط لإظهار اسم الحيوان الأليف
+    pet_name = serializers.ReadOnlyField(source='pet.pet_name')
+    
     class Meta:
         model = Vaccination
-        fields = "__all__"
-        read_only_fields = ['pet']
+        fields = ['id', 'vacc_name', 'vacc_date', 'vacc_certificate', 'pet', 'pet_name']
+        extra_kwargs = {
+            # إخفاء حقل 'pet' من الإدخال لكن إتاحته للعرض
+            'pet': {'write_only': True}
+        }
