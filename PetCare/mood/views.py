6f93a630fd02fdb1a30e8071acdcaf4d732856b0
Rahaf-Almarachli <ririkimv@gmail.com>
2025-09-23 13:20:06 -0600

    fe

--- PetCare/mood/views.py
+++ PetCare/mood/views.py
@@ -3,7 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from .serializers import MoodCreateSerializer
-from .models import Mood, Pet
+from .models import Mood
+from pets.models import Pet
 from django.shortcuts import get_object_or_404
 from datetime import timedelta, date
 
